8f8318093286f2e3667b8f34f8f77ed86337583d
ngthanh-du <nguyenthdu6242@gmail.com>
2023-04-07 21:23:52 +0700

    tao connectdb

--- src/connectDB/ConnectDB.java
+++ src/connectDB/ConnectDB.java
@@ -1,4 +1,33 @@
 package connectDB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectDB {
+    public static Connection con = null;
+    private static ConnectDB instance = new ConnectDB();
+    public static ConnectDB getInstance() {
+        return instance;
+    }
+    public void connect() throws SQLException {
+        //kết nối đến sql sever
+        String url = "jdbc:sqlsever://localhost:1433;databaseName=QLBH";
+        String user = "sa";
+        String password = "2222";
+        con = DriverManager.getConnection(url, user, password);
+    }
+    public void disconnect() throws SQLException {
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+
+            }
+        }
+    }
+    public static Connection getConnection() {
+        return con;
+    }
 }
