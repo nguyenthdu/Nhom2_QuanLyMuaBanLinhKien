40f5189212e088d06b703216bd542f0842036415
Thông <121671912+TranMinhThongg@users.noreply.github.com>
2023-04-10 11:40:16 +0700

    Update DanhMucLinhKien.java

--- src/entity/DanhMucLinhKien.java
+++ src/entity/DanhMucLinhKien.java
@@ -1,4 +1,32 @@
 package entity;
 
-public class  DanhMucLinhKien {
+public class DanhMucLinhKien {
+	private int maDanhMuc;
+	private String tenDanhMuc;
+	public int getMaDanhMuc() {
+		return maDanhMuc;
+	}
+	public String getTenDanhMuc() {
+		return tenDanhMuc;
+	}
+	
+	public void setMaDanhMuc(int maDanhMuc) {
+		this.maDanhMuc = maDanhMuc;
+	}
+	public void setTenDanhMuc(String tenDanhMuc) {
+		this.tenDanhMuc = tenDanhMuc;
+	}
+	public DanhMucLinhKien(int maDanhMuc, String tenDanhMuc) {
+		setMaDanhMuc(maDanhMuc);
+		setTenDanhMuc(tenDanhMuc);
+	}
+	public DanhMucLinhKien(){
+		
+	}
+	public DanhMucLinhKien(int ma){
+		setMaDanhMuc(ma);
+	}
+
+	
+	
 }
